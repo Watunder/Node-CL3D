09e642856a5758875c91c45a57993363bf65e2e5
Watunder <vinschen66@gmail.com>
2026-07-20 12:15:07 +0800

    chore: Remove nc-screen dependcy

--- src/share/getDevicePixelRatio.js
+++ src/share/getDevicePixelRatio.js
@@ -3,22 +3,16 @@ import { isBrowser, isNode } from "../utils/environment.js";
 /**
  * @returns {Number} the ratio of the resolution in physical pixels and in pixels for the current display device.
  */
-let getDevicePixelRatioImpl = () => {
-	return;
-};
+let getDevicePixelRatioImpl = () => 1.0;
 
 if (isNode) {
-	await import("nc-screen").then(async (module) => {
-		getDevicePixelRatioImpl = () => {
-			return module.default.getInfo().isRetina && 2.0;
-		};
-	});
+	// TODO
 } else if (isBrowser) {
 	getDevicePixelRatioImpl = () => {
-		return globalThis.devicePixelRatio;
+		return globalThis.devicePixelRatio || 1.0;
 	};
 }
 
 export const getDevicePixelRatio = () => {
-	return getDevicePixelRatioImpl() || 1.0;
+	return getDevicePixelRatioImpl();
 };
